339343ff01f4b964682da35ba2b343aa0d4eca24
KFlash <YOUR EMAIL ADDRESS>
2016-06-17 10:42:21 +0800

    codecov

--- scripts/ci/after.sh
+++ scripts/ci/after.sh
@@ -2,6 +2,9 @@
 
 set -ex -o pipefail
 
+# codecov.io integration
+bash <(curl -s https://codecov.io/bash)
+
 # Upload to coveralls, but don't _fail_ if coveralls is down.
 cat coverage/lcov.info | node_modules/.bin/coveralls || echo "Coveralls upload failed"
 node node_modules/lcov-filter/index.js lcov.info config | ./node_modules/coveralls/bin/coveralls.js && rm -rf ./coverage
