5aa009d80939062bf25f7df1dd70e241895bf766
KFlash <YOUR EMAIL ADDRESS>
2016-06-23 22:11:39 +0800

    improved Karma

--- config/karma.config.js
+++ config/karma.config.js
@@ -9,6 +9,7 @@ module.exports = function (config) {
 		files: [ { pattern: './test/main.js', watched: false } ],
 		preprocessors: { './test/main.js': ['coverage', 'webpack', 'sourcemap'] },
 		frameworks: ['mocha', 'chai', 'sinon', 'source-map-support'],
+		client: { args: ['--grep', config.grep || ''] },
 		excluded: [],
 		webpack: webpackConfig,
 		coverageReporter: {
@@ -21,7 +22,39 @@ module.exports = function (config) {
 				{ type: 'html' }
 			]
 		},
-		webpackServer: { noInfo: true },
+		webpackMiddleware: {
+			noInfo: true,
+			stats: {
+				// With console colors
+				colors: true,
+				// add the hash of the compilation
+				hash: true,
+				// add webpack version information
+				version: false,
+				// add timing information
+				timings: true,
+				// add assets information
+				assets: true,
+				// add chunk information
+				chunks: true,
+				// add built modules information to chunk information
+				chunkModules: false,
+				// add built modules information
+				modules: false,
+				// add also information about cached (not built) modules
+				cached: true,
+				// add information about the reasons why modules are included
+				reasons: false,
+				// add the source code of modules
+				source: true,
+				// add details to errors (like resolving log)
+				errorDetails: true,
+				// add the origins of chunks and chunk merging info
+				chunkOrigins: true,
+				// Add messages from child loaders
+				children: false
+			}
+		},
 		reporters: ['mocha', 'coverage'],
 		mochaReporter: { output: 'autowatch' },
 		port: 9876,
@@ -36,7 +69,7 @@ module.exports = function (config) {
 		singleRun: true
 	}
 
-    if (isCI) {
+	if (isCI) {
 		configuration.reporters.push('coveralls');
 	}
 
