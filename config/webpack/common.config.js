7efe971a247863c46a83fd326cfa0918449e5284
KFlash <YOUR EMAIL ADDRESS>
2016-06-24 10:19:45 +0800

    now uses Webpack 2.0 Beta

--- config/webpack/common.config.js
+++ config/webpack/common.config.js
@@ -1,16 +1,18 @@
 const webpack = require('webpack');
 const path = require('path')
 const ForkCheckerPlugin = require('awesome-typescript-loader').ForkCheckerPlugin;
+const ProgressPlugin = require('webpack/lib/ProgressPlugin.js');
+const HotModuleReplacementPlugin = require('webpack/lib/HotModuleReplacementPlugin.js');
 
 module.exports = {
 
-	 metadata: {
-        title: 'Sikky',
-        baseUrl: '/'
-    },
+	metadata: {
+		title: 'Sikky',
+		baseUrl: '/'
+	},
 	entry: {
-       'sikky': './src/sikky.ts'
-    },
+		'sikky': './src/sikky.ts'
+	},
 	resolve: {
 		extensions: [ '', '.ts', '.tsx', '.js', '.jsx' ],
 		root:  path.join(__dirname, 'src/'),
@@ -20,39 +22,42 @@ module.exports = {
 		preLoaders: [
 			{
 				// All output '.js' files will have any sourcemaps re-processed by 'source-map-loader'
-        test: /\.js$/,
+				test: /\.js$/,
 				loader: 'source-map-loader',
 				exclude: [
-          // exclude the node dependencies
-           path.join(__dirname, 'node_modules'),
-          // and NPM packages that have problems with their sourcemaps
-					 path.join(__dirname, 'node_modules/rxjs'),
-					 path.join(__dirname, 'node_modules/@angular'),
-					 path.join(__dirname, 'node_modules/@ngrx'),
-					 path.join(__dirname, 'node_modules/@angular2-material'),
+					// exclude the node dependencies
+					path.join(__dirname, 'node_modules'),
+					// and NPM packages that have problems with their sourcemaps
+					path.join(__dirname, 'node_modules/rxjs'),
+					path.join(__dirname, 'node_modules/@angular'),
+					path.join(__dirname, 'node_modules/@ngrx'),
+					path.join(__dirname, 'node_modules/@angular2-material'),
 				]
 			}
 		],
 		loaders: [
 			{
-        // load .ts and .tsx files
+				// load .ts and .tsx files
 				test: /\.tsx?$/,
 				loader: 'babel-loader!awesome-typescript-loader',
 				exclude: [/\.(browser|node|e2e)\.ts$/]
 			}
 		],
 		plugins: [
-      // Do type checking in a separate process, so webpack don't need to wait.
+			new HotModuleReplacementPlugin(),
 			new ForkCheckerPlugin(),
-      // Varies the distribution of the ids to get the smallest id length for often used ids.
-			new webpack.optimize.OccurenceOrderPlugin(true)
+			new ProgressPlugin({})
 		]
 	},
 	node: {
-		global: 'window',
+		global: true,
+		process: true,
+		Buffer: false,
 		crypto: 'empty',
 		module: false,
 		clearImmediate: false,
-		setImmediate: false
+		setImmediate: false,
+		clearTimeout: true,
+		setTimeout: true
 	}
 };
