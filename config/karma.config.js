48c2cf75267a6371c2bfc5d2c875f279b286b4de
KFlash <YOUR EMAIL ADDRESS>
2016-06-24 09:47:06 +0800

    simplified karma

--- config/karma.config.js
+++ config/karma.config.js
@@ -1,4 +1,4 @@
-const webpackConfig  = require('./webpack/karma.config');
+const webpackConfig  = require('./webpack/webpack.karma.config');
 const isCI = process.env.CONTINUOUS_INTEGRATION === 'true';
 
 module.exports = function (config) {
@@ -6,7 +6,6 @@ module.exports = function (config) {
 	const configuration = {
 
 		basePath: '../',
-		urlRoot: '/karma/',
 		files: [ { pattern: 'test/main.js', watched: false } ],
 		preprocessors: { 'test/main.js': ['coverage', 'webpack', 'sourcemap'] },
 		frameworks: ['mocha', 'chai', 'sinon', 'source-map-support'],
@@ -26,47 +25,8 @@ module.exports = function (config) {
 				{ type: 'html' }
 			]
 		},
-		webpackMiddleware: {
-			noInfo: true,
-			stats: {
-				// With console colors
-				colors: true,
-				// add the hash of the compilation
-				hash: true,
-				// add webpack version information
-				version: false,
-				// add timing information
-				timings: true,
-				// add assets information
-				assets: true,
-				// add chunk information
-				chunks: true,
-				// add built modules information to chunk information
-				chunkModules: false,
-				// add built modules information
-				modules: false,
-				// add also information about cached (not built) modules
-				cached: true,
-				// add information about the reasons why modules are included
-				reasons: false,
-				// add the source code of modules
-				source: true,
-				// add details to errors (like resolving log)
-				errorDetails: true,
-				// add the origins of chunks and chunk merging info
-				chunkOrigins: true,
-				// Add messages from child loaders
-				children: false
-			}
-		},
+		webpackServer: { noInfo: true },
 		reporters: ['mocha', 'coverage'],
-		specReporter: {
-			// maxLogLines: 5, // limit number of lines logged per test
-			suppressErrorSummary: true, // do not print error summary
-			suppressFailed: false, // do not print information about failed tests
-			suppressPassed: false, // do not print information about passed tests
-			suppressSkipped: true // do not print information about skipped tests
-		},
 		mochaReporter: { output: 'autowatch' },
 		port: 9876,
 		captureTimeout: 60000,
@@ -80,7 +40,7 @@ module.exports = function (config) {
 		singleRun: true
 	}
 
-	if (isCI) {
+    if (isCI) {
 		configuration.reporters.push('coveralls');
 	}
 
