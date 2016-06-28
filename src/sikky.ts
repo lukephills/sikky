cab5a090bfef96fd7ac992cd262d9568c7747141
Luke Phillips <luke@femurdesign.com>
2016-06-28 13:54:22 +1200

    upgraded webpack-dev-server - HMR and tests work

--- src/sikky.ts
+++ src/sikky.ts
@@ -2,14 +2,13 @@ export default (x: number, y: number): number => x + y;
 
 const app: HTMLElement | null = document.getElementById('app');
 if (app) {
-  app.innerHTML = '<h1>Hello World!</h1>'
+  app.innerHTML = '<h1>Hello World!</h1>';
 }
-
 
 /**
  * HMR allows modules to replace on updates
  */
-declare var module: any; //TODO: add webpack HMR typings
+declare var module: any; // TODO: add webpack HMR typings
 if (module.hot) {
-	module.hot.accept();
+  module.hot.accept();
 }
